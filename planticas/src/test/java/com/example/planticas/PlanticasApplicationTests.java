f505e3b9b5af96ba1ea5b9977364444c79f83196
RAACSO <jgamboasana@gmail.com>
2023-04-25 16:06:47 -0500

    :sparkle:
    first push, base del proyecto y base de datos creada y conectada

---
+++ planticas/src/test/java/com/example/planticas/PlanticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
